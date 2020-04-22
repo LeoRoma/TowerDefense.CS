1303df09fb454aeb68741c51b2d93ac446989986
LeoRoma <xiajtn@gmail.com>
2020-04-22 15:10:45 +0100

    encapsulated the MapLocation array in class Path

--- TowerDefense/Path.cs
+++ TowerDefense/Path.cs
@@ -6,5 +6,10 @@ namespace TowerDefense
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
     }
 }
