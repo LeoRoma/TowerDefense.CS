5faf69e3ee1c99edccd4776f86718de69f739c61
LeoRoma <xiajtn@gmail.com>
2020-04-22 11:33:15 +0100

    instance variables and constructor added to class Point

--- TowerDefense/Point.cs
+++ TowerDefense/Point.cs
@@ -6,5 +6,13 @@ namespace TowerDefense
 {
     class Point
     {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
