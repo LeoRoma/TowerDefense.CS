d80b1eb386f368593a12180429b522f67dc5dc9e
LeoRoma <xiajtn@gmail.com>
2020-04-22 10:12:05 +0100

    class Invader, Map, Path added

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -6,5 +6,10 @@ namespace TowerDefense
 {
     class Game
     {
+        public void Main()
+        {
+            Console.WriteLine("hello");
+            Tower tower = new Tower();
+        }
     }
 }
