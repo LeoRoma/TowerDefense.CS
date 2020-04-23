026117e025d9c5b49760e7b6569a1f51a0d0d4dc
LeoRoma <xiajtn@gmail.com>
2020-04-23 11:43:28 +0100

    changed magic numbers in Tower class with private const

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -6,6 +6,9 @@ namespace TowerDefense
 {
     class Tower
     {
+        private const int _range = 1;
+        private const int _power = 1;
+
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
@@ -17,9 +20,9 @@ namespace TowerDefense
         {
            foreach(Invader invader in invaders)
             {
-                if(invader.IsActive && _location.InRangeOf(invader.Location, 1))
+                if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(1);
+                    invader.DecreaseHealth(_power);
                     break;
                 }
             }
