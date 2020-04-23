4a39b2b54283a7f60748c0a380890992ce3f3a54
LeoRoma <xiajtn@gmail.com>
2020-04-23 12:42:25 +0100

    implemented Random in Tower class

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -8,6 +8,9 @@ namespace TowerDefense
     {
         private const int _range = 1;
         private const int _power = 1;
+        private const double _accuracy = .75;
+
+        private static readonly Random _random = new Random();
 
         private readonly MapLocation _location;
 
@@ -16,12 +19,29 @@ namespace TowerDefense
             _location = location;
         }
 
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
         public void FireOnInvaders(Invader[] invaders)
         {
            foreach(Invader invader in invaders)
             {
                 if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
+                    if(IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader");
+                        if(invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Invader Neutralized!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Miss");
+                    }
                     invader.DecreaseHealth(_power);
                     break;
                 }
