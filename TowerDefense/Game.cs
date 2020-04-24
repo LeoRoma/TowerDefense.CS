9e29fbf06c0a3a179cb4fdca2094a311daf4b0e4
LeoRoma <xiajtn@gmail.com>
2020-04-24 13:07:58 +0100

    created a ResurrectingInvader class with composition

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -26,17 +26,13 @@ namespace TowerDefense
 
                 MapLocation location = new MapLocation(0, 2, map);
 
-                if(path.IsOnPath(location))
-                {
-                    Console.WriteLine(location + " is on path");
-                }
-
-                Invader[] invaders =
+                IInvader[] invaders =
                 {
                     new ShieldedInvader(path),
                     new FastInvader(path),
                     new StrongInvader(path),
-                    new BasicInvader(path)
+                    new BasicInvader(path),
+                    new ResurrectingInvader(path)
                 };
 
                 Tower[] towers =
